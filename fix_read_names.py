ae81329c014478a4391021f84eb87ad8620236f6
Erik Jagyugya <jagyugyaerik@gmail.com>
2017-12-27 21:25:32 +0100

    fix_read_names.py added to normalize reads headers

---
+++ fix_read_names.py
@@ -0,0 +1,78 @@
+from sys import argv, exit
+from re import search
+
+
+def load_fastq(input):
+    reads = []
+
+    with open(input) as fastq:
+        for line in fastq:
+            name = line.strip()[1:]
+            seq = next(fastq).strip()
+            _ = next(fastq)
+            qual = next(fastq).strip()
+            reads.append([name, seq, qual])
+    return reads
+
+
+def main():
+    input_reads_filename = argv[1]
+    read_id_to_fast5_filename = argv[2]
+
+    print("\nReading " + read_id_to_fast5_filename)
+    read_id_to_fast5, fast5_to_read_id = {}, {}
+    with open(read_id_to_fast5_filename, "rt") as read_id_to_fast5_file:
+        read_id_to_fast5_file.readline()
+        for line in read_id_to_fast5_file.readlines():
+            read_id, fast5, _, _ = line.split('\t')
+            if fast5.endswith(".fast5"):
+                fast5 = fast5[:-6]
+            if read_id in read_id_to_fast5_file:
+                exit("Error: duplicate read ID in: " + read_id_to_fast5_filename + ": " + read_id)
+            if fast5 in read_id_to_fast5_file:
+                exit("Error: duplicate fast5 file in: " + read_id_to_fast5_filename + ": " + fast5)
+            read_id_to_fast5[read_id] = fast5
+            fast5_to_read_id[fast5] = read_id
+
+    print("\nLoading reads from: " + input_reads_filename)
+    reads = load_fastq(input_reads_filename)
+
+    print("\nFixing read names...")
+    output_reads = []
+
+
+    for header, seq, qual in reads:
+        read_id, fast5_name = None, None
+        try:
+            read_id = search(r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", header).group(0)
+        except AttributeError:
+            read_id = None
+        try:
+            fast5_name = search(r'\w+_ch\d+_read\d+_\w+', header).group(0)
+        except AttributeError:
+            fast5_name = None
+        if read_id is None and fast5_name is None:
+            exit('Error: could not parse read header\n' + header)
+
+        if read_id is not None:
+            new_header = read_id + ' ' + read_id_to_fast5[read_id]
+        else:
+            new_header = fast5_to_read_id[fast5_name] + ' ' + fast5_name
+
+        output_reads.append([new_header, seq, qual])
+
+    print("\nSorting reads...")
+    output_reads = sorted(output_reads)
+
+    print("\nWrite reads to file...")
+    for header, seq, qual in output_reads:
+        print('@' + header)
+        print(seq)
+        print('+')
+        print(qual)
+
+    print("\nDone")
+
+
+if __name__ == '__main__':
+    main()
