3e0092ab92c53fb35d02d904a961ad24738e35e6
Erik Jagyugya <jagyugyaerik@gmail.com>
2017-12-28 14:41:34 +0100

    refract

--- find_read_names.py
+++ find_read_names.py
@@ -5,22 +5,17 @@ def main():
     read_list = []
     common_reads = []
 
-
-    with open("02_basecalled_reads/chiron.fastq") as fq_file:
-        data = fq_file.readlines()
-        for line in data:
+    with open("03_read_information/chiron.fastq") as fq_file:
+        for line in fq_file:
             if line.startswith('@'):
-                read_list.append(line[1:-1])
+                read_list.append(line.split(' ')[0][1:])
 
     with open(read_info, "rt") as read_file:
-        header = read_file.readline()
-        reads = read_file.readlines()
-        for read in reads:
-            sread = read.split('\t')
-            if sread[1] in read_list:
-                common_reads.append([sread[0], sread[1]])
+        for read in read_file:
+            read_id, fast5 = read.split('\t')
+            if read_id in read_list:
+                common_reads.append([read_id, fast5[:-1]])
 
-    print('\t'.join(["Name", "Fast5 name"]))
     for row in common_reads:
         print('\t'.join(row))
 
