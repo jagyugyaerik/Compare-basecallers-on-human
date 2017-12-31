eed53422571c937c38ce48190ad4ae515e12e705
Erik Jagyugya <jagyugyaerik@gmail.com>
2017-12-31 14:03:43 +0100

    Create common reads from reads in folder and write out for new folder

--- extract_common_reads.py
+++ extract_common_reads.py
@@ -1,27 +1,40 @@
 from sys import argv
+from get_read_info import find_fast5_files
 
 def main():
-    fastq = argv[1]
-    common_reads = argv[2]
+    fixed_reads = argv[1]
 
-    common_id = []
-    common_fast5 = []
-    with open(common_reads) as reads:
-        for line in reads:
-            common_id.append(line.split('\t')[0])
-            common_fast5.append(line.split('\t')[1])
+    common_reads = []
 
-    with open(fastq) as fastq:
-        for line in fastq:
-            if line.split(' ')[0][1:] in common_id:
-                seq = next(fastq).strip()
-                next(fastq)
-                qual = next(fastq).strip()
-                print('@' + line[:-1])
-                print(seq)
-                print('+')
-                print(qual)
+    reads_list = find_fast5_files(argv[1], ".fastq")
 
+    for read in reads_list:
+        read_ids = []
+        with open(read) as fastq:
+            for line in fastq:
+                if line.startswith('@'):
+                    read_ids.append(line[1:].strip().split(' ')[0])
+        if common_reads == []:
+            common_reads = read_ids
+        else:
+            common_reads = list(set(common_reads).intersection(read_ids))
+
+    for read in reads_list:
+        out_read = []
+        with open(read) as fastq:
+            for line in fastq:
+                if line.startswith('@'):
+                    read_id = line[1:].strip().split()[0]
+                    if read_id in common_reads:
+                        seq = next(fastq)
+                        _ = next(fastq)
+                        qual = next(fastq)
+                        out_read.append([line, seq, _, qual])
+
+            with open("04_extract_reads/" + read.split('/')[-1], "wt") as out:
+                for row in out_read:
+                    for r in row:
+                        out.write(r)
 
 if __name__ == '__main__':
     main()
